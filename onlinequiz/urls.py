6261ec45cde1eb2f1afb9284ccf2cd64a6fa599a
samuelakanni <samuelakanni757@yahoo.com>
2020-12-18 13:42:41 +0100

    first comment

--- onlinequiz/urls.py
+++ onlinequiz/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path,include
 from django.contrib import admin
 from quiz import views
-from django.contrib.auth.views import LogoutView,LoginView
+from django.contrib.auth.views import LogoutView, LoginView
+from django.conf import settings
+
 urlpatterns = [
    
     path('admin/', admin.site.urls),
-    path('teacher/',include('teacher.urls')),
-    path('student/',include('student.urls')),
+    path('teacher/', include('teacher.urls')),
+    path('student/', include('student.urls')),
     
 
 
-    path('',views.home_view,name=''),
-    path('logout', LogoutView.as_view(template_name='quiz/logout.html'),name='logout'),
+    path('', views.home_view,name=''),
+    path('logout', LogoutView.as_view(template_name='quiz/logout.html'), name='logout'),
     path('aboutus', views.aboutus_view),
     path('contactus', views.contactus_view),
     path('afterlogin', views.afterlogin_view,name='afterlogin'),
