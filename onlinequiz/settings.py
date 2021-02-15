6fdf69557e9a42c9898c505aec05e852f27f6aeb
samuelakanni <samuelakanni757@yahoo.com>
2021-02-16 00:45:51 +0100

    Allowed Hosts

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -27,10 +27,7 @@ SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '127.0.0.1'
-    'deanakademy.herokuapp.com'
-]
+ALLOWED_HOSTS = ["127.0.0.1", "deanakademy.herokuapp.com"]
 
 
 # Application definition
