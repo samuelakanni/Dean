82eca09bdc5d7131a361a17daf939bcdfb32fe91
samuelakanni <samuelakanni757@yahoo.com>
2021-02-16 10:44:44 +0100

    Allowed hosts

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -157,3 +157,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
